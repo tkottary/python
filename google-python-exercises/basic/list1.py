70a34dad46a48be052bf57ce8163cecefce9d90b
Tarun <tkottary@gmail.com>
2017-01-07 14:48:25 -0800

    list1 updates

--- google-python-exercises/basic/list1.py
+++ google-python-exercises/basic/list1.py
@@ -22,7 +22,11 @@
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
   # +++your code here+++
-  return
+  count =0
+  for str in words :
+      if len(str) >=2 and (str[0] == str[-1:]):
+       count = count+1
+  return count
 
 
 # B. front_x
@@ -33,8 +37,17 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
+
   # +++your code here+++
-  return
+  listA =[]
+  listB= []
+  for str in words:
+      if str[0] == 'x':
+        listA.append(str)
+      else:
+        listB.append(str)
+
+  return sorted(listA) + sorted(listB)
 
 
 
@@ -46,8 +59,11 @@ def front_x(words):
 # Hint: use a custom key= function to extract the last element form each tuple.
 def sort_last(tuples):
   # +++your code here+++
-  return
 
+
+  return sorted(tuples,key =last)
+
+def last(s) : return s[-1]
 
 # Simple provided test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
@@ -75,7 +91,7 @@ def main():
   test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
        ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
 
-       
+
   print
   print 'sort_last'
   test(sort_last([(1, 3), (3, 2), (2, 1)]),
